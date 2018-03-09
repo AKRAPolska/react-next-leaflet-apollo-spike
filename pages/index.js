01f30b70b0e4813a0d900faaeae3816c9e51f80f
Bartlomiej Slipek <bslipek@gmail.com>
2018-03-09 14:28:16 +0100

    Add styled components to project

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,7 @@
-import Head from 'next/head';
-import App from './App';
-//import '../node_modules/leaflet/dist/leaflet.css'
-//import node_modules/leaflet-draw/dist/leaflet.draw.css
+import { createGlobalStyle } from "styled-components";
 
-const Index = () => (
-  <div>
-    <Head>
-      <title>Leaflet_AKRA</title>
-      <link href="https://unpkg.com/leaflet@1.3.1/dist/leaflet.css" rel="stylesheet" />
-      <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet.draw/0.4.2/leaflet.draw.css" />
-    </Head>
-    <style jsx global>{`
-       .sr-only {
-        display: none;
-      }
-    `}</style>
-        <App />
-        
-  </div>
-)
+import FullScreenMap from "./map";
 
-export default Index
+const Index = () => <FullScreenMap />;
+
+export default Index;
